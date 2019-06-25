f47f901f82d96da969fa05755ce9f557220f0cd2
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-06-25 22:58:58 +0400

    Add launch power indicator (view)

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pinball
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        public Transform maskBar;
+
+        private float _defaultOffset;
+        private float _defaultScale;
+
+        private float _offset
+        {
+            get => maskBar.localPosition.y;
+            set
+            {
+                var v = maskBar.localPosition;
+                v.y = value;
+                maskBar.localPosition = v;
+            }
+        }
+
+        private float _scale
+        {
+            get => maskBar.localScale.y;
+            set
+            {
+                var v = maskBar.localScale;
+                v.y = value;
+                maskBar.localScale = v;
+            }
+        }
+
+        private void Awake()
+        {
+            Debug.Assert(maskBar != null);
+
+            _defaultOffset = _offset;
+            _defaultScale = _scale;
+        }
+
+        // progress: [0; 1]
+        public void SetProgress(float progress)
+        {
+            _scale = _defaultScale * (1 - progress);
+            _offset = _defaultOffset + (_defaultScale / 2 * progress);
+        }
+    }
+}
