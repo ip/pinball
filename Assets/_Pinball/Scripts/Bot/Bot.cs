5d91c6fc8e1e664d53b7d25b7239bfb7aa375973
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-06-30 20:24:54 +0400

    Bot: trigger flippers

--- Assets/_Pinball/Scripts/Bot/Bot.cs
+++ Assets/_Pinball/Scripts/Bot/Bot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,8 +11,10 @@ namespace Pinball
     {
         public bool isLaunchStarted;
         public bool isLaunchEnded;
+        public bool[] isSidePressed =
+            new bool[Enum.GetNames(typeof(InputSide)).Length];
 
-        public bool IsSidePressed(InputSide side) => false;
+        public bool IsSidePressed(InputSide side) => isSidePressed[(int)side];
         public bool IsLaunchStarted() => isLaunchStarted;
         public bool IsLaunchEnded() => isLaunchEnded;
     }
@@ -19,22 +22,49 @@ namespace Pinball
     [RequireComponent(typeof(InputProvider), typeof(BallLauncher))]
     public class Bot : MonoBehaviour
     {
+        private struct FlipperInfo
+        {
+            public Vector2 position;
+            public InputSide side;
+        }
+
+        public Transform ball;
+
+        // A flipper will be triggered when the distance between the ball
+        // and the flipper axis falls into this interval
+        public float triggerMin = 2;
+        public float triggerMax = 3.87f;
+
         public float minLaunchSpeed = 47.7f;
 
+        private BallLauncher _ballLauncher;
         private BotInput _input = new BotInput();
-
         private bool _isActive;
         private bool _isLaunching;
-
-        private BallLauncher _ballLauncher;
+        private FlipperInfo[] _flippersInfo;
 
         private void Awake()
         {
             _ballLauncher = GetComponent<BallLauncher>();
 
+            Debug.Assert(ball != null);
             Debug.Assert(minLaunchSpeed <= _ballLauncher.maxSpeed);
 
+            _SubscribeToPlayStateChange();
+            _CacheFlippersInfo();
+        }
 
+        private void Update()
+        {
+            if (!_isActive)
+                return;
+
+            _HandleBallLaunch();
+            _TriggerFlippersIfNeeded();
+        }
+
+        private void _SubscribeToPlayStateChange()
+        {
             var inputProvider = GetComponent<InputProvider>();
             EventManager.instance.OnGameStart += (bool isBotMode) =>
             {
@@ -47,14 +77,6 @@ namespace Pinball
 
             EventManager.instance.OnGameOver += () =>
                 _isActive = false;
-        }
-
-        private void Update()
-        {
-            if (!_isActive)
-                return;
-
-            _HandleBallLaunch();
         }
 
         private void _HandleBallLaunch()
@@ -83,6 +105,37 @@ namespace Pinball
 
             yield return new WaitForSeconds(1);
             _isLaunching = false;
+        }
+
+        private void _CacheFlippersInfo()
+        {
+            _flippersInfo = MonoBehaviour.FindObjectsOfType<Flipper>()
+                .Select(flipper => new FlipperInfo
+                {
+                    position = (Vector2)flipper.transform.position,
+                    side = flipper.inputSide,
+                })
+                .ToArray();
+        }
+
+        private void _TriggerFlippersIfNeeded()
+        {
+            bool[] isSidePressed = _input.isSidePressed;
+            Vector2 ballPos = ball.transform.position;
+
+            for (int j = 0; j < isSidePressed.Length; ++j)
+                isSidePressed[j] = false;
+
+            for (int i = 0; i < _flippersInfo.Length; ++i)
+            {
+                var flipper = _flippersInfo[i];
+
+                float distance = Vector2.Distance(flipper.position, ballPos);
+                bool shouldTrigger = distance >= triggerMin &&
+                                     distance <= triggerMax;
+                if (shouldTrigger)
+                    isSidePressed[(int)flipper.side] = true;
+            }
         }
     }
 }
